e87be5296b012b2563e106e41022f907a1a6e76b
Morgan D <124601263+mDean1@users.noreply.github.com>
2023-11-22 18:41:21 -0600

    Additional forest levels

--- Woerm/Assets/Scripts/SettingsMenu.cs
+++ Woerm/Assets/Scripts/SettingsMenu.cs
@@ -25,7 +25,7 @@ public class SettingsMenu : MonoBehaviour
         List<string> options = new List<string>();
 
         for (int i = 0; i < resolutions.Length ; i++){
-            string option = resolutions[i].width + " x " + resolutions[i].height + " @ " + resolutions[i].refreshRate + "hz";            
+            string option = resolutions[i].width + " x " + resolutions[i].height + " @ " + resolutions[i].refreshRateRatio + "hz";            
             options.Add(option);
             
             if(resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height){
